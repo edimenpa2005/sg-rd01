2e1ed07ae7446fcadc100d403d4adca3b6b12f07
edimenpa2005 <edimenpa2005@gmail.com>
2013-10-16 14:23:33 -0500

    commit 03
    Se introduce el codigo para responsive desing

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Estructura Basica semantica en html5 </title>
+	<meta charset="utf-8" />
+	<meta name="description" content="Estructura basica semantica en html5 y css3 con responsive desing"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+</head>
+
+<body>
+
+<header>
+	<!-- Encabezado tiene que tiene que tener 3 divs-->
+	
+</header>
+<nav>
+	&lt;nav&gt;
+</nav>
+<section id="contenedor">
+	<section id="principal">
+		&lt;section&gt;	
+		<article>
+			&lt;article&gt;
+		</article>
+
+	</section>
+	<aside>&lt;article&gt; </aside>
+</section>
+<footer>
+	&lt;footer&gt;
+</footer>
+</body>
+</html>
