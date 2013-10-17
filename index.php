3fdd601fb1fe3001ba799476c9b603aac051b48a
edimenpa2005 <edimenpa2005@gmail.com>
2013-10-17 14:38:00 -0500

    commit 04
    Se empieza a trabajar css de accuerdo al wireframe y se incluye fondo,
    logo y fuente roboto

--- index.php
+++ index.php
@@ -12,12 +12,55 @@
 
 <header>
 	<!-- Encabezado tiene que tiene que tener 3 divs-->
-	
+	<?php 
+	// Los identificadores y las clases se parecen, usamos identificadores para dar propiedades a un solo elemento, y clases para un dar propiedades a un grupo de elementos
+
+	 ?>
+<!--Nesecitamos 3 divs para el header	-->
+<div id="logo">
+	<a href="#"><img src="css/img/logo.png" width="75" height="75" alt="Colegio San Gabriel" id="ilogo"></a>
+</div>
+<div id="titulo"><h1>Colegio Privado San Gabriel</h1></div>
+<div id="identificacion"><p>Usted a ingresado como</p></div>
+
+
 </header>
 <nav>
-	&lt;nav&gt;
+	<ul>
+		<li>Nuestro Colegio</li>
+		<li>Servicios que ofrecemos</li>
+		<li>Admision</li>
+
+	</ul>
 </nav>
+<div id="slide">
+	<p>slide en jquery</p>
+</div>
 <section id="contenedor">
+	<section id="principal">
+		&lt;section&gt;	
+		<article>
+			&lt;article&gt;
+		</article>
+
+	</section>
+	<aside>&lt;article&gt; </aside>
+	<section id="principal">
+		&lt;section&gt;	
+		<article>
+			&lt;article&gt;
+		</article>
+
+	</section>
+	<aside>&lt;article&gt; </aside>
+	<section id="principal">
+		&lt;section&gt;	
+		<article>
+			&lt;article&gt;
+		</article>
+
+	</section>
+	<aside>&lt;article&gt; </aside>
 	<section id="principal">
 		&lt;section&gt;	
 		<article>
